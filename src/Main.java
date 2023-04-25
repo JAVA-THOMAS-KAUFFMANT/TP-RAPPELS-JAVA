46d53a86f8753d6d7e8242f8ae5ff67d6fe37974
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-25 11:07:01 +0200

    feat: finish tp 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        gameMultiplicator();
     }
 
-    public static void gameMulpiplication() {
+    public static void gameMultiplicator() {
         boolean game = true;
+
         while(game) {
             // Generate random number
             Random r = new Random();
@@ -20,16 +26,49 @@ public class Main {
 
             Scanner in = new Scanner(System.in);
 
-            int s = Integer.parseInt(in.nextLine());
+            String s = in.nextLine();
 
-            multiplication felix = new multiplication();
-            boolean result = felix.checkResult(numberOne, numberTwo, s);
-
-            if (result == true) {
-                System.out.println("Correct !");
+            if (s.equals("stop") || s.equals("Stop") || s.equals("STOP")) {
+                game = false;
+                break;
             } else {
-                System.out.println("Incorrect...");
+                multiplication felix = new multiplication();
+                boolean result = felix.checkResult(numberOne, numberTwo, Integer.parseInt(s));
+
+                if (result == true) {
+                    System.out.println("Correct !");
+                } else {
+                    System.out.println("Incorrect...");
+                }
             }
         }
+    };
+
+    public static void writeInAFile() {
+        // Create a file
+        try {
+            File myObj = new File("thomasFile.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        // Write in a file :
+        try {
+            FileWriter myWriter = new FileWriter("thomasFile.txt");
+            myWriter.write("Ceci est un test, Thomas test file");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
     };
 }
